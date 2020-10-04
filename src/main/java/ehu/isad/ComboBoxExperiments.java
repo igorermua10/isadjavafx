0ba1180c483f580e3affc2ee7c9a6b0e64424ad6
igor <igortxu@hotmail.es>
2020-10-04 21:23:47 +0200

    akats batzuk konponduta eta bateratuta

--- src/main/java/ehu/isad/ComboBoxExperiments.java
+++ src/main/java/ehu/isad/ComboBoxExperiments.java
@@ -7,7 +7,6 @@ import javafx.stage.Stage;
 import com.google.gson.Gson;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -34,9 +33,9 @@ public class ComboBoxExperiments extends Application  {
 
         comboBox.setOnAction(e -> {
             System.out.println( comboBox.getValue());
-            String emaitza = this.getString((String)comboBox.getValue());
+            String emaitza = this.URLReader((String)comboBox.getValue());
             Txanpona txanpona = gson.fromJson(emaitza, Txanpona.class);
-            labela.setText("1 "+(String)comboBox.getValue()+"= "+txanpona.price);
+            labela.setText("1 "+(String)comboBox.getValue()+"= "+txanpona.prezioa);
         });
 
         VBox vbox = new VBox(labela, comboBox);
@@ -45,9 +44,22 @@ public class ComboBoxExperiments extends Application  {
         primaryStage.show();
 
     }
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    private String URLReader(String moneta) throws Exception {
 
+        URL monetaWebOrri = new URL("https://api.gdax.com/products/"+moneta+"-eur/ticker");
+        URLConnection hasi = monetaWebOrri.openConnection();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(monetaWebOrri.openStream()));
+
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+
+        return inputLine;
+
+    }
 
 
     public static void main(String[] args) {
@@ -56,24 +68,7 @@ public class ComboBoxExperiments extends Application  {
     public class Txanpona {
         float prezioa;
     }
-    public class URLReader {
-        public static String URLReader(String moneta) throws Exception {
-            Gson gson = new Gson();
-            URL monetaWebOrri = new URL("https://api.gdax.com/products/"+moneta+"-eur/ticker");
-            URLConnection hasi = monetaWebOrri.openConnection();
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(monetaWebOrri.openStream()));
 
-            String inputLine;
-
-            while ((inputLine = in.readLine()) != null)
-                System.out.println(inputLine);
-            in.close();
-
-            return inputLine;
-
-        }
-    }
 }
 
 
